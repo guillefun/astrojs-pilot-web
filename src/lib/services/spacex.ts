b5d6c1ab9cc3d08fae2be70928339bf923c7c77e
Guillermo Fernández-Campoamor Fernández <guillermofcampoamor@gmail.com>
2024-02-12 23:14:11 +0100

    Detail of flight information

--- src/lib/services/spacex.ts
+++ src/lib/services/spacex.ts
@@ -1,4 +1,5 @@
-import type { APIXSpaceXResponse } from "../../types/api";
+import type { GetStaticPathsResult } from "astro";
+import type { APIXSpaceXResponse, Doc } from "../../types/api";
 
 export const getLatestLaunches = async () => {
     const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
@@ -12,11 +13,39 @@ export const getLatestLaunches = async () => {
                 sort: {
                     date_unix: 'asc'
                 },
-                limit: 12
+                limit: 30
             }
         })
     })
 
     const { docs }= await res.json() as APIXSpaceXResponse;
     return docs;
+}
+
+export const getStaticPathsForLaunches = async () => {
+    const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: {},
+            options: {
+                sort: {
+                    date_unix: 'asc'
+                },
+                limit: 30
+            }
+        })
+    })
+
+    const { docs }= await res.json() as APIXSpaceXResponse;
+    return docs.map((d) => ({ params: { id: d.id } }));
+}
+
+export const getLaunchById = async ({id} : {id: string}) => {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+
+    const launch = await res.json() as Doc;
+    return launch;
 }
